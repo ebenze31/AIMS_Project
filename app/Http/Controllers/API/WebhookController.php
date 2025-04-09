b30188b92b23c8481396ee5dab5adebe2abdafd4
EBenze <52275898+ebenze31@users.noreply.github.com>
2025-04-09 13:27:55 +0700

    Update WebhookController.php

--- app/Http/Controllers/API/WebhookController.php
+++ app/Http/Controllers/API/WebhookController.php
@@ -7,20 +7,20 @@ use Illuminate\Http\Request;
 
 class WebhookController extends Controller
 {
-    public function handle(Request $request)
-    {
-        // รับข้อมูลจาก Third Party
-        $data = $request->all();
+    // public function handle(Request $request)
+    // {
+    //     // รับข้อมูลจาก Third Party
+    //     $data = $request->all();
 
-        // บันทึกลง session อยู่แค่ 1 request
-        session()->flash('webhook_data', $data);
+    //     // บันทึกลง session อยู่แค่ 1 request
+    //     session()->flash('webhook_data', $data);
 
-        return redirect()->route('form_sos');
-        // return response()->json([
-        //     'redirect_url' => route('form_sos')  // ส่ง URL ที่ต้องการให้ทำการ redirect
-        // ]);
+    //     return redirect()->route('form_sos');
+    //     // return response()->json([
+    //     //     'redirect_url' => route('form_sos')  // ส่ง URL ที่ต้องการให้ทำการ redirect
+    //     // ]);
 
-    }
+    // }
 
     public function handle(Request $request)
     {
