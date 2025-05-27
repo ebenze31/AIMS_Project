c2692101d95b5067bee5d7f4a39843b67f918b5e
EBenze <52275898+ebenze31@users.noreply.github.com>
2025-05-27 12:08:35 +0700

    Update Aims_emergencysController.php

--- app/Http/Controllers/Aims_emergencysController.php
+++ app/Http/Controllers/Aims_emergencysController.php
@@ -369,40 +369,40 @@ class Aims_emergencysController extends Controller
             ->select($unit_selects)
             ->first();
 
-        $test_return = [];
-        $test_retur['emergency'] = $emergency;
-        $test_retur['officer'] = $officer;
-        $test_retur['photo'] = $emergency->emergency_photo;
+        // $test_return = [];
+        // $test_retur['emergency'] = $emergency;
+        // $test_retur['officer'] = $officer;
+        // $test_retur['photo'] = $emergency->emergency_photo;
 
-        return $test_retur;
+        // return $test_retur;
 
         $template_path = storage_path('../public/json/aims/send_sos.json');
         $string_json = file_get_contents($template_path);
 
         $string_json = str_replace("ตัวอย่าง","การขอความช่วยเหลือ",$string_json);
 
-        if (!empty( $emergency->emergency_photo )) {
-            $string_json = str_replace("photo_sos.png",$emergency->emergency_photo,$string_json);
-        }
+        // if (!empty( $emergency->emergency_photo )) {
+        //     $string_json = str_replace("photo_sos.png",$emergency->emergency_photo,$string_json);
+        // }
 
-        $string_json = str_replace("name_user",$emergency->name_reporter,$string_json);
-        $string_json = str_replace("area",$emergency->area_name_area,$string_json);
-        $string_json = str_replace("หัวข้อขอความช่วยเหลือ",$emergency->emergency_type,$string_json);
-        $string_json = str_replace("รายละเอียดขอความช่วยเหลือ",$emergency->emergency_detail,$string_json);
+        // $string_json = str_replace("name_user",$emergency->name_reporter,$string_json);
+        // $string_json = str_replace("area",$emergency->area_name_area,$string_json);
+        // $string_json = str_replace("หัวข้อขอความช่วยเหลือ",$emergency->emergency_type,$string_json);
+        // $string_json = str_replace("รายละเอียดขอความช่วยเหลือ",$emergency->emergency_detail,$string_json);
 
-        $string_json = str_replace("png_language","-",$string_json);
-        $string_json = str_replace("png_national","-",$string_json);
-        $string_json = str_replace("0999999999","-",$string_json);
+        // $string_json = str_replace("png_language","-",$string_json);
+        // $string_json = str_replace("png_national","-",$string_json);
+        // $string_json = str_replace("0999999999","-",$string_json);
 
-        $string_json = str_replace("วันที่แจ้ง",$date_now,$string_json);
-        $string_json = str_replace("เวลาที่แจ้ง",$time_now,$string_json);
+        // $string_json = str_replace("วันที่แจ้ง",$date_now,$string_json);
+        // $string_json = str_replace("เวลาที่แจ้ง",$time_now,$string_json);
 
-        $string_json = str_replace("emergency_id",$emergency_id,$string_json);
-        $string_json = str_replace("aims_area_id",$emergency->aims_area_id,$string_json);
+        // $string_json = str_replace("emergency_id",$emergency_id,$string_json);
+        // $string_json = str_replace("aims_area_id",$emergency->aims_area_id,$string_json);
 
-        $string_json = str_replace("gg_lat_mail",$text_at.$lat_user,$string_json);
-        $string_json = str_replace("gg_lat",$lat_user,$string_json);
-        $string_json = str_replace("lng",$lng_user,$string_json);
+        // $string_json = str_replace("gg_lat_mail",$text_at.$lat_user,$string_json);
+        // $string_json = str_replace("gg_lat",$lat_user,$string_json);
+        // $string_json = str_replace("lng",$lng_user,$string_json);
 
         $messages = [ json_decode($string_json, true) ];
 
