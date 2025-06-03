9b9ac0bd62468079a8294c82719eaa7286ed8052
EBenze <52275898+ebenze31@users.noreply.github.com>
2025-06-03 17:25:58 +0700

    Update LineApiController.php

--- app/Http/Controllers/API/LineApiController.php
+++ app/Http/Controllers/API/LineApiController.php
@@ -837,23 +837,7 @@ class LineApiController extends Controller
             ]);
 
 
-        $columns = Schema::getColumnListing('aims_emergency_operations');
-        $selects = array_map(function ($col) {
-            return "aims_emergency_operations.$col as op_$col";
-        }, $columns);
-
-        $emergency = DB::table('aims_emergencys')
-            ->where('aims_emergencys.id', '=', $emergency_id)
-            ->leftJoin('aims_emergency_operations', 'aims_emergencys.id', '=', 'aims_emergency_operations.aims_emergency_id')
-            ->leftJoin('aims_areas', 'aims_emergencys.aims_area_id', '=', 'aims_areas.id')
-            ->leftJoin('aims_partners', 'aims_emergencys.aims_partner_id', '=', 'aims_partners.id')
-            ->select(array_merge(
-                ['aims_emergencys.*'],
-                $selects,
-                ['aims_areas.name_area as area_name_area'],
-                ['aims_partners.name as partner_name']
-            ))
-            ->first();
+        
 
         $template_path = storage_path('../public/json/aims/data_text.json');
         $string_json = file_get_contents($template_path);
