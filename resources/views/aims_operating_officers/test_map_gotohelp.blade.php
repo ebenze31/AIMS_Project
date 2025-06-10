df13b73293fec3793e9c4bd5bdb52116c57506d0
EBenze <52275898+ebenze31@users.noreply.github.com>
2025-06-10 17:36:12 +0700

    Update test_map_gotohelp.blade.php

--- resources/views/aims_operating_officers/test_map_gotohelp.blade.php
+++ resources/views/aims_operating_officers/test_map_gotohelp.blade.php
@@ -1129,40 +1129,7 @@ function open_map() {
         heading: currentHeading,
         mapId: "90f87356969d889c",
     });
-    const buttons = [
-        ["Rotate Left", "rotate", 20, google.maps.ControlPosition.LEFT_CENTER],
-        ["Rotate Right", "rotate", -20, google.maps.ControlPosition.RIGHT_CENTER],
-        ["Tilt Down", "tilt", 20, google.maps.ControlPosition.TOP_CENTER],
-        ["Tilt Up", "tilt", -20, google.maps.ControlPosition.BOTTOM_CENTER],
-    ];
-
-    buttons.forEach(([text, mode, amount, position]) => {
-        const controlDiv = document.createElement("div");
-        const controlUI = document.createElement("button");
-
-        controlUI.classList.add("ui-button");
-        controlUI.innerText = `${text}`;
-        controlUI.addEventListener("click", () => {
-            adjustMap(mode, amount);
-        });
-        controlDiv.appendChild(controlUI);
-        map.controls[position].push(controlDiv);
-    });
-
-    const adjustMap = function (mode, amount) {
-        switch (mode) {
-            case "tilt":
-                map.setTilt(map.getTilt() + amount);
-                break;
-            case "rotate":
-                currentHeading = (map.getHeading() + amount + 360) % 360;
-                map.setHeading(currentHeading);
-                break;
-            default:
-                break;
-        }
-    };
-
+    
     // Marker สำหรับจุดฉุกเฉิน
     emergencyMarker = new google.maps.Marker({
         position: emergency_LatLng,
